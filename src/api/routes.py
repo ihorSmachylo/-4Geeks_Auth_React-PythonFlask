db35be4cde437c17c7c3ebeb8d3dc0ef37d71b67
ihorSmachylo <igoor.smachyloo@gmail.com>
2023-05-02 16:07:33 +0000

    AOUTH done

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,14 +5,43 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/signup", methods=["POST"])
+def signup():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if not email or not password:
+        return jsonify({"msg": "An username and password must be entered."}), 400
+    check_if_exist = User.query.filter_by(email = email).first()
+    if check_if_exist:
+        return jsonify({"msg": "Email in use"})
+    user = User(email = email,
+                password = password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"username_added": email}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email = email).first()
+    if user:
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "User not found"}), 404
 
-    return jsonify(response_body), 200
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
